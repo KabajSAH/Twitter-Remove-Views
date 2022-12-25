c3a8e075f7bde7388eb2ab53b535b57a27bf0e7f
Yanis SI AHMED HADDI <yanissiahmedhaddi@gmail.com>
2022-12-25 01:36:28 +0100

    Changed selector to improve the durability

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,12 @@
-//document.querySelectorAll("div.css-1dbjc4n.r-18u37iz.r-1h0z5md>a").forEach( e => e.parentNode.remove());
+const Better_SEL = 'a[href*="/analytics"]';
 
-const Main_TL_SEL = 'div.css-1dbjc4n.r-18u37iz.r-1h0z5md>a';
-const Focus_Tweet_SEL = 'div.css-1dbjc4n.r-2sztyj.r-1efd50x.r-5kkj8d.r-13awgt0.r-18u37iz.r-tzz3ar.r-s1qlax.r-1yzf0co>div.css-1dbjc4n.r-1mf7evn>div.css-1dbjc4n>a';
-const Focus_Tweet_Regex = new RegExp('^.*\/analytics$');
 const mo = new MutationObserver(onMutation);
 observe();
 
 function onMutation() {
-  if (document.querySelector(Main_TL_SEL)) {
+  if (document.querySelector(Better_SEL)) {
     mo.disconnect();
-    deleteMainTLViews();
-    observe();
-  }
-  if (document.querySelector(Focus_Tweet_SEL)) {
-    mo.disconnect();
-    deleteFocusTweetViews();
+    deleteBetterViews();
     observe();
   }
 }
@@ -26,22 +18,18 @@ function observe() {
   });
 }
 
-function deleteMainTLViews(){
-  const viewCountElements = document.querySelectorAll(Main_TL_SEL)
-  if(viewCountElements){
-    viewCountElements.forEach(e => e.parentNode.remove());
-  }
-}
 
-function deleteFocusTweetViews(){
-  const viewCountElements = document.querySelectorAll(Focus_Tweet_SEL)
+function deleteBetterViews(){
+  const viewCountElements = document.querySelectorAll(Better_SEL);
   if(viewCountElements){
     viewCountElements.forEach(e => {
-
-      if(e.href.match(Focus_Tweet_Regex))
-        e.parentNode.parentNode.remove()
-
-
+      if(e.dataset.testid !== 'analyticsButton'){
+        let elem = e;
+        while(!elem.nextSibling){
+          elem = elem.parentNode;
+        }
+        elem.remove();
+      }
     });
   }
 }
